039acba54f89d09ae5a22a0bc4b85036b5011cc7
ADITYA VAISHAMPAYAN <aditya1.vaishampayan@gmail.com>
2019-11-02 14:17:33 -0400

    remove the fashion-mnist folder

--- project/main.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from matplotlib import pyplot as plt
-import seaborn as sns
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
-from utils import mnist_reader
-from matplotlib import pyplot as plt
