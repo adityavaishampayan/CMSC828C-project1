048f5e06ed3db2b8f3f36d9b59ff2656bcf8f531
ADITYA VAISHAMPAYAN <aditya1.vaishampayan@gmail.com>
2019-11-10 13:59:54 -0500

    update setup file

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
 from setuptools import setup, find_packages
+from os import path
 
-with open("README.md", "r") as readme_file:
-    readme = readme_file.read()
+here = path.abspath(path.dirname(__file__))
 
+# get the dependencies and installs
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+    
 requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
 
 setup(
     name="CMSC_828C_Project1",
     version="0.0.1",
+    description = 'Python implementation of fundamenta Machine Learning algorithms on Fashion-MNIST dataset',
+    url='https://github.com/adityavaishampayan/CMSC828C-project1.git'
+    license='LICENSE.txt',
+    packages = find_packages(),
     author="Aditya Vaishampayan",
     author_email="adityav@terpmail.umd.edu",
-    description="A project that contains bayes classifier implementation ",
     long_description=readme,
     long_description_content_type="text/markdown",
     install_requires=requirements,
-    license='LICENSE.txt',
+    setup_requires=['numpy>=1.15', 'scipy>=0.17','matplotlib>=3.0.0','scikit-learn=0.20.2']
+    
 )
